2bbe5ba3c9bd73c94ea7744ba5d7eb1080380a03
EkatLiskova <liskova98@mail.ru>
2024-04-12 22:27:10 +0500

    Создание анонимного пользователя При аутентификации определятся свой собственный тест И мини фиксы

--- cattest/views.py
+++ cattest/views.py
@@ -15,14 +15,23 @@ from saitik3.settings import COUNT_OF_QUESTION
 
 class RegisterViews(APIView):
     def post(self, request):
-        name = request.data.get('name') #при регистрации
-        chek = User.objects.all().filter(username=name)
-        print(chek)
-        mail = ''
-        password = request.data.get('password')
-        if chek:
-            return render(request, 'регистрация.html')
-        User.objects.create_user(name, mail, password)
+        button_answer = request.data.get('button_answer')
+        if button_answer == 1:
+            name = request.data.get('name') #при регистрации
+            chek = User.objects.all().filter(username=name)
+            password = request.data.get('password')
+            if chek:#TODO: сделать в html уведомление о том, что такой логин уже существует
+                return render(request, 'регистрация.html')
+        else:#cоздание анонимного пользователя #TODO: сделать, чтобы анонимус удалялся
+            colvo_of_user = len(User.objects.all())
+            name = 'Anonim' + str(colvo_of_user)
+            chek1 = User.objects.all().filter(username=name)
+            if chek1:
+                name = name + 'povtor'
+            password = name
+        User.objects.create_user(name, '', password)# '' это типа маил
+        id = User.objects.all().get(username=name).id
+        Test.objects.create(type_of_kotik=None, current_question=1, user_id=id, answers='')
         user = authenticate(request, username=name, password=password)
         login(request, user)
         return redirect('Начало')
@@ -33,28 +42,29 @@ class RegisterViews(APIView):
 
 class QuestionViews(APIView):
 
-    def get_question(self):
-        user_id = 1
-        number_of_question = Test.objects.all().get(user_id=user_id).current_question
+    def get_question(self, request):
+        id = User.objects.all().get(username=request.user).id
+        number_of_question = Test.objects.all().get(user_id=id).current_question
         text_of_question = Question.objects.all().get(number_of_question=number_of_question).text_of_question
         return {'textik': text_of_question}
 
     def get(self, request):
-        user_id = 1
-        user_test_info = Test.objects.all().get(user_id=user_id)
+        id = User.objects.all().get(username=request.user).id
+        user_test_info = Test.objects.all().get(user_id=id)
         if user_test_info.current_question > COUNT_OF_QUESTION:
             return redirect('Результат')
-        return render(request, 'Прохождение теста.html', context=self.get_question())
+        return render(request, 'Прохождение теста.html', context=self.get_question(request))
 
     def post(self, request):
-        user_id = 1
-        user_test_info = Test.objects.all().get(user_id=user_id)
+        id = User.objects.all().get(username=request.user).id
+        user_test_info = Test.objects.all().get(user_id=id)
         button_answer = request.data.get('button_answer')
         user_test_info.answers = user_test_info.answers + button_answer
         user_test_info.current_question = user_test_info.current_question + 1
         user_test_info.save()
+        print(request.user)
         if user_test_info.current_question <= COUNT_OF_QUESTION:
-            return render(request, 'Прохождение теста.html', context=self.get_question())
+            return render(request, 'Прохождение теста.html', context=self.get_question(request))
         else:
             #тут будет вычисляться результат
             return redirect('Результат')
@@ -76,11 +86,11 @@ def result(answers):
     is_active = True if active.count('1') > 3 else False
     is_communication = True if communication.count('1') > 3 else False
 
+
 class StartViews(APIView):
     def get(self, request): #request это любые данные, которые передаются в запросе на сервер
-
-        user_id = 1
-        user_test_info = Test.objects.all().get(user_id=user_id)
+        id = User.objects.all().get(username=request.user).id
+        user_test_info = Test.objects.all().get(user_id=id)
         if user_test_info.current_question > COUNT_OF_QUESTION:
             return redirect('Результат')
         elif user_test_info.current_question > 1:
@@ -94,8 +104,8 @@ class ResultView(APIView):
         return render(request, 'Результат теста.html')
 
     def post(self, request):
-        user_id = 1
-        user_test_info = Test.objects.all().get(user_id=user_id)
+        id = User.objects.all().get(username=request.user).id
+        user_test_info = Test.objects.all().get(user_id=id)
         user_test_info.answers = ''
         user_test_info.current_question = 1
         user_test_info.type_of_kotik = None
@@ -116,6 +126,7 @@ class LoginView(APIView):
 
     def get(self, request):
         return render(request, 'вход.html')
+
 
 class CheckView(APIView):
     def get(self, request):
